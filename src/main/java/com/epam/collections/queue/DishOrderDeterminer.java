31b0d93f5885172dc781b66cd1a11a2a4e111d55
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-13 17:03:53 +0300

    feat: start Queue Interface Task module4 stage1 v.0

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,11 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+        return  new ArrayList<>();
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,12 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayDeque;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+
+        return new PriorityQueue<>();
     }
 }
