6d14c81285ae642f35b7e367c65292d7817177ca
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-16 11:56:07 +0300

    feat: complete determineDishOrder module4 stage1 v.0

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,11 +1,25 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
 
-        return  new ArrayList<>();
+        List<Integer> dishes = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishes.add(i);
+        }
+
+        ArrayList<Integer> eatingOrder = new ArrayList<>();
+        int currentIndex = 0;
+        while (!dishes.isEmpty()) {
+            currentIndex = (currentIndex + everyDishNumberToEat - 1) % dishes.size();
+            eatingOrder.add(dishes.get(currentIndex));
+            dishes.remove(currentIndex);
+        }
+
+        return eatingOrder;
     }
 }
