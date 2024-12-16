454080cdb0498dbb338240c4f5d66e282ed05b4a
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-16 09:51:21 +0300

    feat: complete PriorityQueueCreator module4 stage1 v.0

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,11 +1,14 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+
 
         return new ArrayDeque<>();
     }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,12 +1,15 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
 
-        return new PriorityQueue<>();
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        priorityQueue.addAll(firstList);
+        priorityQueue.addAll(secondList);
+
+        return priorityQueue;
     }
 }
